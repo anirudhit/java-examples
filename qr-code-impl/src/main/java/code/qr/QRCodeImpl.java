43bb42e6ef22bb567db527c547d5d039a9b363b9
anirudh bellamkonda <hyanirudh@gmail.com>
2020-01-23 16:51:03 -0600

    qr-code-impl
    - Added code for qr code generation using string and saving it to jpg file.

--- qr-code-impl/src/main/java/code/qr/QRCodeImpl.java
+++ qr-code-impl/src/main/java/code/qr/QRCodeImpl.java
@@ -1,5 +1,25 @@
 package code.qr;
 
-public class QRCodeImpl {
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
+public class QRCodeImpl {
+	public static void main(String[] args) throws IOException {
+		
+		String details = "Anirudh Bellamkonda -> https://www.banirudh.com";
+		ByteArrayOutputStream out = QRCode.from(details).to(ImageType.JPG).stream();
+		
+		File f = new File("C:\\Anirudh\\github\\Java\\java-examples\\qr-code-impl\\src\\main\\resources\\MyWebsite.jpg");
+		FileOutputStream fos = new FileOutputStream(f);
+		
+		fos.write(out.toByteArray());
+		fos.flush();
+		
+		fos.close();
+	}
 }
